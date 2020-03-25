e63a36ce0c830b90416a90ea4dafe6f20ff42a90
Michael Lang <michael.lang@nexul.com>
2020-03-25 18:08:05 -0500

    Create the API fileController for uploads and downloads of files

---
+++ Nexul.Demo.MvcWeb/ApiControllers/FileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nexul.Demo.MvcWeb.ApiControllers
+{
+    /// <summary>
+    /// Allows download of file metadata, file content, and uploading files.
+    /// </summary>
+    [Route("api/file")]
+    public class FileController : ControllerBase
+    {
+        //TODO: add action methods here.
+    }
+}
