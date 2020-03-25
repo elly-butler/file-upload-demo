f4959e3103466886b2a2a9e25dffc11cc47f28b0
Michael Lang <michael.lang@nexul.com>
2020-03-25 17:59:53 -0500

    Added Api web project and SQL repository implementation project (minimal changes)

---
+++ Nexul.Demo/Nexul.Demo.SqlDbServices/SqlFileData.cs
@@ -0,0 +1,39 @@
+﻿using Nexul.Demo.Files;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nexul.Demo.SqlDbServices
+{
+    public class SqlFileData : IFileData
+    {
+        public File GetFile(string fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileMetadata GetFileMetadata(string fileId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FileMetadata> GetUserFiles(string userId, string skipPastId = null, int take = 50)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertFile(File item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFile(File updated, File existing)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(FileMetadata metadata)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
