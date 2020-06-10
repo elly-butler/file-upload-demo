470289d087ea0ca5fd395fed7e1c36f43c935874
Michael Lang <michael.lang@nexul.com>
2020-06-10 15:08:52 -0500

    ng g m item --routing --route item --module app

--- nexul-demo-cli/src/app/app-routing.module.ts
+++ nexul-demo-cli/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'item', loadChildren: () => import('./item/item.module').then(m => m.ItemModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
